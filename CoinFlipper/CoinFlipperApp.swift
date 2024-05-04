944d45c646bf4ae6e24124cebe8bd96c066447fa
Quercy <alexghezha@gmail.com>
2024-05-04 22:15:34 +0300

    Initial Commit

---
+++ CoinFlipper/CoinFlipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlipperApp.swift
+//  CoinFlipper
+//
+//  Created by Quercy on 04.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinFlipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
